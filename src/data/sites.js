a30aefb05766cf7266bc2bb9a2f910b36a5242b3
Trevor <litseyt@me.com>
2018-10-17 12:15:29 -0500

    add blue  sites

--- src/data/sites.js
+++ src/data/sites.js
@@ -1,9 +1,69 @@
+import bars from '../images/bars.gif'
+import tri from '../images/tri.png'
+import shade from '../images/shade.gif'
+import bigSmall from '../images/bigsmall.gif'
+import noFun from '../images/no-fun.gif'
+import zoom from '../images/zoom.gif'
+import speck from '../images/speck.gif'
+import buzz from '../images/buzz.gif'
+import grid from '../images/grid.gif'
+import float from '../images/float.gif'
+import iHopeThisFeelingLasts from '../images/i-hope-this-feeling-lasts.gif'
+
 export const blues = [
   {
-    href: 'https: //trebor.blue/bars',
-    imgSrc:
-      'https://res.cloudinary.com/sadness/image/upload/v1525134507/trevorlitsey.com/bars.gif',
-    alt: 'bars',
+    href: 'https://trebor.blue/bars',
+    imgSrc: bars,
     label: 'bars',
+  },
+  {
+    href: 'https://trebor.blue/tri',
+    imgSrc: tri,
+    label: 'bars',
+  },
+  {
+    href: 'https://trebor.blue/shade',
+    imgSrc: shade,
+    label: 'shade',
+  },
+  {
+    href: 'https://trebor.blue/bigsmall',
+    imgSrc: bigSmall,
+    label: 'big small',
+  },
+  {
+    href: 'https://trebor.blue/nofun',
+    imgSrc: noFun,
+    label: 'no fun',
+  },
+  {
+    href: 'https://trebor.blue/zoom',
+    imgSrc: zoom,
+    label: 'zoom',
+  },
+  {
+    href: 'https://trebor.blue/speck',
+    imgSrc: speck,
+    label: 'speck',
+  },
+  {
+    href: 'https://trebor.blue/buzz',
+    imgSrc: buzz,
+    label: 'buzz',
+  },
+  {
+    href: 'https://trebor.blue/grid',
+    imgSrc: grid,
+    label: 'grid',
+  },
+  {
+    href: 'https://trebor.blue/float',
+    imgSrc: float,
+    label: 'float',
+  },
+  {
+    href: 'https://trebor.blue/i-hope-this-feeling-lasts',
+    imgSrc: iHopeThisFeelingLasts,
+    label: 'i hope this feeling lasts',
   },
 ]
