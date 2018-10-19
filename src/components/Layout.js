ea3e2302fb47f9efcad83fc02dfbe770ea8528bb
Trevor <litseyt@me.com>
2018-10-19 12:43:02 -0500

    update meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,8 +21,24 @@ const Layout = ({ children, ...props }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { charset: 'UTF-8' },
+            {
+              name: 'description',
+              content: 'Personal website of Trevor Litsey',
+            },
+            { name: 'keywords', content: 'trevor litsey' },
+            { httpEquiv: 'Content-Type', content: 'text/html; charset=utf-8' },
+            { httpEquiv: 'X-UA-Compatible', content: 'ie=edge' },
+            {
+              name: 'viewport',
+              content: 'width=device-width, initial-scale=1.0',
+            },
+            { property: 'og:title', content: 'Trevor Litsey' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content: 'http://trevorlitsey.com/' },
+            { property: 'og:image', content: '/img/josef.png' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:site_name', content: 'trevorlitsey.com' },
           ]}
         >
           <html lang="en" />
